53d1183fd239824889b231a5a62a30461f6303e0
sicktooth <sosaronungwe@gmail.com>
2024-02-14 06:09:46 +0100

    update game.js

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-alert("hello computer");
+buttonColors = ["red", "blue", "green", "yellow"];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
