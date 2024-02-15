07fdc9140db08fb9453028bd782c2f40837402c3
sicktooth <sosaronungwe@gmail.com>
2024-02-15 06:20:56 +0100

    update game.js

--- game.js
+++ game.js
@@ -1,7 +1,44 @@
-buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 
+buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     return randomNumber;
 }
+
+var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor);
+
+var selectedId = $("#" + randomChosenColor);
+
+selectedId.fadeOut(100).fadeIn(100);
+
+switch (selectedId) {
+    case "#green":
+        var green = new Audio('sounds/green.mp3');
+        green.play();
+    break;
+
+    case "#blue":
+        var blue = new Audio('sounds/blue.mp3');
+        blue.play();
+    break;
+
+    case "#yellow":
+        var yellow = new Audio('sounds/yellow.mp3');
+        yellow.play();
+    break;
+
+    case "#red":
+        var red = new Audio('sounds/red.mp3');
+        red.play();
+    break;
+
+    default:
+        var wrong = new Audio('sounds/wrong.mp3');
+        wrong.play();
+    break;
+}
+
