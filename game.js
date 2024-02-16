16668aa1c3861e6688b1677df72059b1df06e268
sicktooth <sosaronungwe@gmail.com>
2024-02-16 06:11:00 +0100

    updated game.js

--- game.js
+++ game.js
@@ -15,6 +15,7 @@ var selectedId = $("#" + randomChosenColor);
 
 selectedId.fadeOut(100).fadeIn(100);
 
+
 switch (selectedId) {
     case "#green":
         var green = new Audio('sounds/green.mp3');
@@ -40,5 +41,26 @@ switch (selectedId) {
         var wrong = new Audio('sounds/wrong.mp3');
         wrong.play();
     break;
+} 
+/*
+if (selectedId == "#green") {
+    var audio = new Audio('sounds/green.mp3');
+} else if (selectedId == "#blue") {
+    var audio = new Audio('sounds/blue.mp3');
+} else if (selectedId == "#yellow") {
+    var audio = new Audio('sounds/yellow.mp3');
+} else if (selectedId == "#red") {
+    var audio = new Audio('sounds/red.mp3');
+} else {
+    var audio = new Audio('sounds/wrong.mp3');
 }
+audio.play(); 
+*/
 
+$(".btn").on("click", function(e){
+    
+    var userChosenColor = e.target;
+
+    console.log(userChosenColor);
+
+});
