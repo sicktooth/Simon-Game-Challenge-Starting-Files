306c6147b9e2bf887924600a3649217161224915
sicktooth <sosaronungwe@gmail.com>
2024-02-18 19:17:02 +0100

    update game.js

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ var userClickedPattern = [];
 
 var gamePattern = [];
 
+
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     // return randomNumber;
@@ -18,14 +20,39 @@ function nextSequence() {
 
     playSound(randomChosenColor);
 
+    // $("#level-title").text("Level " + level);
+
+    // for (var i = 0; i < 10; i++) {
+    //     // var level = i;
+    //     $("#level-title").text("Level " + i);
+    // }
+
 }
 
-nextSequence();
+
+
+var firstKeyDown = true;
+
+$(document).keydown(function (e) {
+    if (firstKeyDown) {
+        nextSequence();
+        console.log(e);
+        firstKeyDown = false;
+    }
+    
+});
 
 
 function playSound(name) {
     var audio = new Audio ('sounds/' + name + '.mp3');
     audio.play();
+}
+
+function animatePress(currentColor) {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(function() {
+        $('#' + currentColor).removeClass('pressed');
+    }, 100);
 }
 
 $(".btn").on("click", function(){
@@ -37,5 +64,9 @@ $(".btn").on("click", function(){
     console.log(userClickedPattern);
 
     playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+
+    nextSequence();
 
 });
