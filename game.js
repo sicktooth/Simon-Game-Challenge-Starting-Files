1c2d7a351a7f1297abb441a60c3fdc8a6db70cf7
sicktooth <sosaronungwe@gmail.com>
2024-02-24 03:24:28 +0100

    done fixing it

--- game.js
+++ game.js
@@ -6,6 +6,46 @@ var gamePattern = [];
 
 var level = 0;
 
+$(".btn").on("click", function(){
+    
+    var userChosenColor = $(this).attr('id');
+
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+    
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() { 
+                nextSequence();
+            }, 1000);
+        }
+
+    }
+    else {
+        startOver();
+    }
+}
+
+function playSound(name) {
+    var audio = new Audio ('sounds/' + name + '.mp3');
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(function() {
+        $('#' + currentColor).removeClass('pressed');
+    }, 100);
+}
+
 function nextSequence() {
 
     userClickedPattern = [];
@@ -55,42 +95,3 @@ function startOver() {
 }
 
 
-function playSound(name) {
-    var audio = new Audio ('sounds/' + name + '.mp3');
-    audio.play();
-}
-
-function animatePress(currentColor) {
-    $('#' + currentColor).addClass('pressed');
-    setTimeout(function() {
-        $('#' + currentColor).removeClass('pressed');
-    }, 100);
-}
-
-$(".btn").on("click", function(){
-    
-    var userChosenColor = $(this).attr('id');
-
-    userClickedPattern.push(userChosenColor);
-
-    playSound(userChosenColor);
-
-    animatePress(userChosenColor);
-    
-    checkAnswer(userChosenColor.length-1);
-});
-
-function checkAnswer(currentLevel) {
-    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-
-        if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() { 
-                nextSequence();
-            }, 1000);
-        }
-
-    }
-    else {
-        startOver();
-    }
-}
